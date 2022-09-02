1c15879ea47a5fb507d02daff4fe8afe17269738
Brandon <100369868+Holtguitar@users.noreply.github.com>
2022-09-01 23:14:09 -0400

    Mobile remastering

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,99 @@
-// smooth scroll
-const headerLinks = ".container";
+////////////// smooth scroll//////////////////
+const headerLinks = ".navbar-nav";
+const brand = ".brand-container";
 
 function smoothScroll(el) {
   document.querySelector(el).addEventListener("click", function (e) {
     e.preventDefault();
-
-    // if (
-    //   e.target.classList.contains("nav-link") ||
-    //   e.target.classList.contains(".brand-img")
-    // ) {
     const id = e.target.getAttribute("href");
     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
-    // }
   });
 }
 
+smoothScroll(brand);
 smoothScroll(headerLinks);
+
+//////////////////// mobile navbar toggler //////////////////
+const hamburger = document.querySelector(".navbar-toggler");
+const navBarCollapse = document.querySelector(".navbar-collapse");
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const skillset = document.querySelector("#skillset");
+const portfolio = document.querySelector("#portfolio");
+const contact = document.querySelector("#contact");
+const homeLink = document.getElementById("nav-home");
+const aboutLink = document.getElementById("nav-about");
+const skillsetLink = document.getElementById("nav-skillset");
+const portfolioLink = document.getElementById("nav-portfolio");
+const contactLink = document.getElementById("nav-contact");
+const navRefs = [home, about, skillset, portfolio, contact];
+const navLinks = [
+  homeLink,
+  aboutLink,
+  skillsetLink,
+  portfolioLink,
+  contactLink,
+];
+
+let collapsed = true;
+let extraOffSet = 0;
+
+const extraOffSetter = function () {
+  if (collapsed) {
+    extraOffSet = 0;
+  } else {
+    extraOffSet = 270;
+  }
+};
+
+function toggleCollapse() {
+  collapsed = !collapsed;
+  extraOffSetter();
+  setTimeout(() => {
+    navBarCollapse.classList.toggle("show");
+  }, "150");
+}
+
+hamburger.addEventListener("click", toggleCollapse);
+
+window.addEventListener("scroll", function () {
+  let scroll = window.scrollY + extraOffSet;
+
+  const removeActive = function () {
+    navLinks.forEach((el) => {
+      el.classList.remove("active-link");
+    });
+  };
+
+  if (scroll >= home.offsetTop && scroll < about.offsetTop) {
+    // console.log(scroll - extraOffSet);
+    removeActive();
+    homeLink.classList.add("active-link");
+  } else if (scroll >= about.offsetTop && scroll <= skillset.offsetTop) {
+    removeActive();
+    aboutLink.classList.add("active-link");
+  } else if (scroll >= skillset.offsetTop && scroll <= portfolio.offsetTop) {
+    removeActive();
+    skillsetLink.classList.add("active-link");
+  } else if (scroll >= portfolio.offsetTop && scroll <= contact.offsetTop) {
+    removeActive();
+    portfolioLink.classList.add("active-link");
+  } else if (scroll >= contact.offsetTop) {
+    removeActive();
+    contactLink.classList.add("active-link");
+  }
+});
+
+homeLink.addEventListener("click", toggleCollapse);
+//////////////////// Change header based on scroll position //////////////////
+let sectionTwo = document.querySelector("#header-carousel");
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", function () {
+  let scroll = window.scrollY;
+  if (scroll > sectionTwo.offsetTop) {
+    navbar.classList.add("bg-light");
+  } else {
+    navbar.classList.remove("bg-light");
+  }
+});
